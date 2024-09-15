bba70d4f50e612ed36d15b395821b24b3df40db7
Terry Verduijn <t.verduijn@hotmail.com>
2024-09-15 07:23:40 +0200

    fixed login with spotify

---
+++ components/login-button.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Button } from './ui/button'
+import { FaSpotify } from 'react-icons/fa6'
+import { createClient } from '@/utils/supabase/client'
+
+const handleSpotifyLogin = async () => {
+  const supabase = createClient()
+
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'spotify',
+    options: {
+      scopes: 'user-read-email playlist-modify-public playlist-modify-private',
+      redirectTo: 'http://localhost:3000/auth/callback', // Redirect after login
+    },
+  })
+  if (error) {
+    console.error('Error logging in with Spotify:', error.message)
+  }
+}
+
+export default function LoginButton() {
+  return (
+    <Button size='sm' onClick={handleSpotifyLogin}>
+      <FaSpotify className='mr-2 text-chart-2' />
+      Sign in with Spotify
+    </Button>
+  )
+}
