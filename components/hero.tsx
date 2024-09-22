82328bbf53402894a5611b70205fea45b3772180
Terry Verduijn <t.verduijn@hotmail.com>
2024-09-22 22:33:35 +0200

    fixed hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,12 +1,10 @@
 export default function Hero() {
   return (
     <div className='justify-center'>
-      <section className='max-w-4xl relative overflow-hidden bg-gray-50'>
+      <section className='max-w-4xl relative overflow-hidden'>
         <div className='container mx-auto px-4 py-16 text-center'>
-          <h1 className='text-4xl font-extrabold text-gray-900 sm:text-6xl'>
-            Cadence Beats
-          </h1>
-          <p className='mt-6 text-xl text-gray-600'>
+          <h1 className='text-4xl font-extrabold sm:text-6xl'>Cadence Beats</h1>
+          <p className='mt-6 text-xl'>
             Sync your Strava activities with Spotify! Generate playlists that
             match your running or cycling cadence. Elevate your workouts with
             beats tailored to your pace!
